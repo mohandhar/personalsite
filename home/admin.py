7dfdf348dc684f35e86e2e8313560d8f52d3f54e
Mohan Dhar <mohan@albert.com>
2021-08-08 14:58:37 -0400

    Added Education, Location, Attendance Models

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Person
+from .models import Person, Education, Attendance, Location
 
-admin.site.register(Person)
+admin.site.register(Person, Education, Attendance, Location)
--- home/models.py
+++ home/models.py
@@ -8,3 +8,23 @@ class Person(models.Model):
     email_address = models.EmailField()
     phone_number = PhoneNumberField(unique=True, null=False, blank=False)
     description = models.CharField(max_length=256)
+
+
+class Location(models.Model):
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+
+
+class Attendance(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+
+
+class Education(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    school_name = models.CharField(max_length=50)
+    location = Location()
+    attendance = Attendance()
+    degree = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    gpa = models.DecimalField(max_digits=3, decimal_places=2)
