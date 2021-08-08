ca5f52681bbdf63cbe6af914c1bb0a3e7032cad0
Mohan Dhar <mohan@albert.com>
2021-08-08 19:46:43 -0400

    Added skeletoncss and reworked the frontend

--- home/views.py
+++ home/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+import logging
+
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, render
+
+from .models import Person
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
     """Home page of the website"""
-    context = dict()
+    person = get_object_or_404(
+        Person.objects,
+        first_name='Mohan',
+        last_name='Dhar',
+    )
+    logger.info('Person: ', person)
+    context = dict(
+        person=person
+    )
     return render(request, 'home/index.html', context)
