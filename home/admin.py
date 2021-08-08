df15f2a133da4dacc26a0d00d08c376810cce6b5
Mohan Dhar <mohan@albert.com>
2021-08-08 15:02:08 -0400

    Register the Education, Location, Attendance, and Person models correctly

--- home/admin.py
+++ home/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Person, Education, Attendance, Location
 
-admin.site.register(Person, Education, Attendance, Location)
+admin.site.register(Attendance)
+admin.site.register(Education)
+admin.site.register(Location)
+admin.site.register(Person)
