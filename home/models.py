7c4f39d5618d26747462f3d03fbcf9c67f868439
Mohan Dhar <mohan@albert.com>
2021-08-08 15:47:04 -0400

    Added unique constraints to Attendance and Location

--- home/models.py
+++ home/models.py
@@ -14,17 +14,29 @@ class Location(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['city', 'state'], name='unique_location')
+        ]
+
 
 class Attendance(models.Model):
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['start_date', 'end_date'], name='unique_attendance')
+        ]
+
 
 class Education(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     school_name = models.CharField(max_length=50)
-    location = Location()
-    attendance = Attendance()
+    location = models.ForeignKey(Location, on_delete=models.RESTRICT)
+    attendance = models.ForeignKey(Attendance, on_delete=models.RESTRICT)
     degree = models.CharField(max_length=50)
     major = models.CharField(max_length=50)
     gpa = models.DecimalField(max_digits=3, decimal_places=2)
